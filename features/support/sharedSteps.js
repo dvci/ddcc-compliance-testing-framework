9b73d7c02039c737e9f4a80e760396a97462d823
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2021-11-23 19:45:01 -0500

    add fhir validator step

--- features/support/sharedSteps.js
+++ features/support/sharedSteps.js
@@ -1,8 +1,20 @@
+const pactum = require('pactum');
+const { expression } = require('pactum-matchers');
 const {
-  Given
+  Given, Then
 } = require('@cucumber/cucumber');
 
 // eslint-disable-next-line prefer-arrow-callback
 Given(/I set json body to the file at (.*)$/, function (fixture) {
   this.spec.withJson(fixture);
+});
+
+Then(/I expect response should validate against the profile (.*)$/, async function (profile) {
+  const response = await pactum.spec()
+    .post(`http://localhost:4567/validate?profile=${profile}`)
+    // eslint-disable-next-line no-underscore-dangle
+    .withJson(this.spec._response.json);
+  pactum.expect(response).to.have.status(200);
+  pactum.expect(response).to.have.jsonMatch('issue[*].severity', expression(null, '!$V.includes("fatal")'));
+  pactum.expect(response).to.have.jsonMatch('issue[*].severity', expression(null, '!$V.includes("error")'));
 });
