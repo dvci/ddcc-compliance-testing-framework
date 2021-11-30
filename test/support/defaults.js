6148f4e5da153560de31e757a3a57fbf8283135e
mickohanlon23 <mohanlon@mitre.org>
2021-11-30 10:51:14 -0500

    Move mock interactions from test/support.default.js to test/support/mockConfig.js. Create more specific pactum matching for mock request bodies

--- test/support/defaults.js
+++ test/support/defaults.js
@@ -1,11 +1,6 @@
 const { mock } = require('pactum');
 const { Before, BeforeAll, AfterAll } = require('@cucumber/cucumber');
-const { like } = require('pactum-matchers');
 const mockConfig = require('./mockConfig');
-
-const sheResponse = require('../fixtures/Bundle-DDCC-TX-SHE-response-example-1.json');
-const operationOutcomeError = require('../fixtures/OperationOutcome-example.json');
-const ddccDocument = require('../fixtures/Bundle-DDCC-Document-example.json');
 
 BeforeAll(() => {
   // perform some shared setup
@@ -19,82 +14,6 @@ Before(function (testCase, callback) {
       .then(() => callback())
       .catch((error) => callback(error))
   }
-});
-
-Before({ tags: '@SubmitHealthEvent' }, () => {
-  mock.addInteraction([{
-    id: 'submitHealthEvent',
-    strict: false,
-    request: {
-      method: 'POST',
-      path: '/submitHealthEvent',
-      body: {
-        resourceType: like('Bundle'),
-        id: like('DDCC-TX-SHE-bundle-example-1')
-      }
-    },
-    response: {
-      status: 201,
-      body: sheResponse
-    }
-  }]);
-});
-
-Before({ tags: '@SubmitInvalidHealthEvent' }, () => {
-  mock.addInteraction([{
-    id: 'submitHealthEvent',
-    strict: false,
-    request: {
-      method: 'POST',
-      path: '/submitHealthEvent',
-      body: {
-        resourceType: like('Bundle'),
-        id: like('DDCC-TX-SHE-bundle-example-invalid')
-      }
-    },
-    response: {
-      status: 422,
-      body: operationOutcomeError
-    }
-  }]);
-});
-
-Before({ tags: '@GenerateHealthCertificate' }, () => {
-  mock.addInteraction([{
-    id: 'generateHealthCertificate',
-    strict: false,
-    request: {
-      method: 'POST',
-      path: '/QuestionnaireResponse/$generateHealthCertificate',
-      body: {
-        resourceType: like('Parameters'),
-        id: like('DDCC-VS-TX-SHE-Parameters-1')
-      }
-    },
-    response: {
-      status: 201,
-      body: ddccDocument
-    }
-  }]);
-});
-
-Before({ tags: '@GenerateHealthCertificateInvalid' }, () => {
-  mock.addInteraction([{
-    id: 'generateHealthCertificate',
-    strict: false,
-    request: {
-      method: 'POST',
-      path: '/QuestionnaireResponse/$generateHealthCertificate',
-      body: {
-        resourceType: like('Parameters'),
-        id: like('DDCC-VS-TX-SHE-Parameters-1-invalid')
-      }
-    },
-    response: {
-      status: 422,
-      body: operationOutcomeError
-    }
-  }]);
 });
 
 AfterAll(() => {
