259de1e1c9a56adadd4b05e189022253b86e650c
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2021-12-22 15:12:35 -0500

    use jsonLike matcher

--- features/submitHealthEvent.feature
+++ features/submitHealthEvent.feature
@@ -1,15 +1,15 @@
 @GenerationService
+@SubmitHealthEvent
 Feature: Submit Health Event
 
-  @SubmitHealthEvent
+  @SubmitValidHealthEvent
   Scenario: Submit Valid Health Event
     Given I make a POST request to /submitHealthEvent
       And I set json body to the file at ./features/fixtures/submitHealthEvent/Bundle-DDCC-TX-SHE-bundle-example-1.json
      When I receive a response
      Then I expect response should have a status 201
       And I expect response should validate against the profile http://worldhealthorganization.github.io/ddcc/StructureDefinition/DDCCSubmitHealthEventResponse
-      And I expect response should have value Aulus Agerius at path entry[0].resource.entry[fullUrl=http://www.example.org/fhir/Patient/DDCC-Patient-English].resource.name[0].text
-      And I expect fields should be persisted between the request and the repsonse
+      And I expect response should have a json body like the file at ./features/fixtures/submitHealthEvent/SubmitValidHealthEvent-response.json
 
   @SubmitInvalidHealthEvent
   Scenario: Submit Invalid Health Event
--- features/support/sharedSteps.js
+++ features/support/sharedSteps.js
@@ -3,6 +3,7 @@ const { expression } = require('pactum-matchers');
 const {
   Given, Then
 } = require('@cucumber/cucumber');
+const fs = require('fs');
 
 // eslint-disable-next-line prefer-arrow-callback
 Given(/I set json body to the file at (.*)$/, function (fixture) {
@@ -15,12 +16,9 @@ Then(/I expect response should validate against the profile (.*)$/, async functi
   }
 
   const response = await pactum.spec()
-    // .name('Submit Health Event')
     .post(`${this.parameters.validatorServiceUrl}?profile=${profile}`)
     // eslint-disable-next-line no-underscore-dangle
     .withJson(this.spec._response.json);
-    // .expectJsonSnapshot();
-    // .updateSnapshot();
 
   if (response.statusCode !== 200) {
     Promise.reject(new Error(`Error executing FHIR validator service: ${response.statusMessage}`));
@@ -28,7 +26,6 @@ Then(/I expect response should validate against the profile (.*)$/, async functi
 
   pactum.expect(response).to.have.jsonMatch('issue[*].severity', expression(null, '!$V.includes("fatal")'));
   pactum.expect(response).to.have.jsonMatch('issue[*].severity', expression(null, '!$V.includes("error")'));
-  // pactum.expect(response).to.have.jsonSnapshot();
   return Promise.resolve();
 });
 
@@ -36,17 +33,7 @@ Then(/I expect response should have value (.*) at path (.*)/, function (value, p
   this.spec.response().should.have.jsonMatch(path, value);
 });
 
-Then(/I expect fields should be persisted between the request and the repsonse/, function () {
-  // this.spec.response().should.have.jsonMatch({ hello: 'fsdf' });
-  // await pactum.spec()
-  //   .name('Submit Health Event')
-
-  // TODO: Make this dynamic so that it can be inputted via each respoective Gherkin test
-  this.spec.name('Submit Health Event');
-  this.spec.expectJsonSnapshot();
-  // console.log(this.spec.response().should.have.response.jsonSnapshot)
-  // this.spec.should.have.jsonSnapshot();
-  // console.lothis.spec.response().should.have.jsonSnapshot());
-  // this.spec.settings.setSnapshotDirectoryPath('/')
-  // this.spec.updateSnapshot()
+Then(/I expect response should have a json body like the file at (.*)$/, function (fixture) {
+  const json = fs.readFileSync(`${fixture}`);
+  this.spec.response().should.have.jsonLike(JSON.parse(json));
 });
